ac0a77784db514014c73c850600a8849141a66ff
Shipra35 <147172758+Shipra35@users.noreply.github.com>
2024-10-30 16:40:41 +0530

    Final Submission

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
 import pdb
-import src
 import glob
-import importlib
+import importlib.util
 import os
 import cv2
 
-
-
 ### Change path to images here
-path = 'Images{}*'.format(os.sep)  # Use os.sep, Windows, linux have different path delimiters
-###
+path = f'Images{os.sep}*'  # Use os.sep for cross-platform compatibility
 
+# Get all submission folders inside ./src/
 all_submissions = glob.glob('./src/*')
+
+# Create results directory if it doesn't exist
 os.makedirs('./results/', exist_ok=True)
-for idx,algo in enumerate(all_submissions):
-    print('****************\tRunning Awesome Stitcher developed by: {}  | {} of {}\t********************'.format(algo.split(os.sep)[-1],idx,len(all_submissions)))
+
+for idx, algo in enumerate(all_submissions):
+    print(
+        f'****************\tRunning Awesome Stitcher developed by: '
+        f'{algo.split(os.sep)[-1]}  | {idx + 1} of {len(all_submissions)}\t********************'
+    )
     try:
-        module_name = '{}_{}'.format(algo.split(os.sep)[-1],'stitcher')
-        filepath = '{}{}stitcher.py'.format( algo,os.sep,'stitcher.py')
+        # Construct the path to stitcher.py
+        module_name = f"{algo.split(os.sep)[-1]}_stitcher"
+        filepath = f"{algo}{os.sep}stitcher.py"
+
+        # Dynamically load the stitcher module
         spec = importlib.util.spec_from_file_location(module_name, filepath)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
+
+        # Get the PanaromaStitcher class from the loaded module
         PanaromaStitcher = getattr(module, 'PanaromaStitcher')
         inst = PanaromaStitcher()
 
-        ###
+        # Process each image set in the given path
         for impaths in glob.glob(path):
-            print('\t\t Processing... {}'.format(impaths))
+            print(f'\t\t Processing... {impaths}')
+
+            # Run the panorama stitching method
             stitched_image, homography_matrix_list = inst.make_panaroma_for_images_in(path=impaths)
 
-            outfile =  './results/{}/{}.png'.format(impaths.split(os.sep)[-1],spec.name)
-            os.makedirs(os.path.dirname(outfile),exist_ok=True)
-            cv2.imwrite(outfile,stitched_image)
-            print(homography_matrix_list)
-            print('Panaroma saved ... @ ./results/{}.png'.format(spec.name))
-            print('\n\n')
+            # Construct the output path
+            output_dir = f'./results/{impaths.split(os.sep)[-1]}'
+            outfile = f'{output_dir}/{module_name}.png'
+
+            # Create the output directory if it doesn't exist
+            os.makedirs(output_dir, exist_ok=True)
+
+            # Save the stitched panorama
+            cv2.imwrite(outfile, stitched_image)
+
+            print(f'Homography Matrices:\n{homography_matrix_list}')
+            print(f'Panorama saved ... @ {outfile}\n\n')
 
     except Exception as e:
-        print('Oh No! My implementation encountered this issue\n\t{}'.format(e))
-        print('\n\n')
+        print(f'Oh No! My implementation encountered this issue\n\t{e}\n\n')
--- src/JohnDoe/stitcher.py
+++ src/JohnDoe/stitcher.py
@@ -2,41 +2,74 @@ import pdb
 import glob
 import cv2
 import os
-from src.JohnDoe import some_function
-from src.JohnDoe.some_folder import folder_func
+import numpy as np
 
 class PanaromaStitcher():
     def __init__(self):
         pass
 
-    def make_panaroma_for_images_in(self,path):
-        imf = path
-        all_images = sorted(glob.glob(imf+os.sep+'*'))
-        print('Found {} Images for stitching'.format(len(all_images)))
+    def detect_and_match_features(self, img1, img2):
+        # Initialize SIFT detector
+        sift = cv2.SIFT_create()
 
-        ####  Your Implementation here
-        #### you can use functions, class_methods, whatever!! Examples are illustrated below. Remove them and implement yours.
-        #### Just make sure to return final stitched image and all Homography matrices from here
-        self.say_hi()
-        self.do_something()
-        self.do_something_more()
+        # Detect keypoints and compute descriptors
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
 
-        some_function.some_func()
-        folder_func.foo()
+        # Use BFMatcher with L2 norm for SIFT descriptors
+        bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+        matches = bf.match(des1, des2)
 
-        # Collect all homographies calculated for pair of images and return
-        homography_matrix_list =[]
-        # Return Final panaroma
+        # Sort matches by distance (quality of match)
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        return kp1, kp2, matches
+
+    def estimate_homography(self, kp1, kp2, matches):
+        # Extract matched keypoints
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+
+        # Use Direct Linear Transform (DLT) to estimate homography
+        A = []
+        for i in range(len(src_pts)):
+            x, y = src_pts[i][0]
+            u, v = dst_pts[i][0]
+            A.append([-x, -y, -1, 0, 0, 0, u * x, u * y, u])
+            A.append([0, 0, 0, -x, -y, -1, v * x, v * y, v])
+
+        A = np.array(A)
+        _, _, V = np.linalg.svd(A)
+        H = V[-1].reshape(3, 3)
+
+        return H / H[-1, -1]  # Normalize
+
+    def stitch_images(self, img1, img2, H):
+        # Warp the second image to align with the first image
+        height, width = img1.shape[:2]
+        warped_img2 = cv2.warpPerspective(img2, H, (width * 2, height))
+
+        # Combine the two images into a panorama
+        panorama = np.copy(warped_img2)
+        panorama[0:height, 0:width] = img1
+
+        return panorama
+
+    def make_panaroma_for_images_in(self, path):
+        all_images = sorted(glob.glob(path + os.sep + '*'))
+        print(f'Found {len(all_images)} images for stitching.')
+
         stitched_image = cv2.imread(all_images[0])
-        #####
-        
-        return stitched_image, homography_matrix_list 
+        homography_matrix_list = []
 
-    def say_hi(self):
-        print('Hii From John Doe..')
-    
-    def do_something(self):
-        return None
-    
-    def do_something_more(self):
-        return None
+        for i in range(1, len(all_images)):
+            img1 = stitched_image
+            img2 = cv2.imread(all_images[i])
+
+            kp1, kp2, matches = self.detect_and_match_features(img1, img2)
+            H = self.estimate_homography(kp1, kp2, matches)
+
+            stitched_image = self.stitch_images(img1, img2, H)
+            homography_matrix_list.append(H)
+
+        return stitched_image, homography_matrix_list
